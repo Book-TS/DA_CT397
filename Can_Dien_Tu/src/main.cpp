2b36e1c77bc8983badbf1348f1f4eee53632350b
BookTS <sachtran1811@gmail.com>
2023-05-08 12:26:30 +0700

    Update - 12:26 08/05/2023

--- Can_Dien_Tu/src/main.cpp
+++ Can_Dien_Tu/src/main.cpp
@@ -1,226 +1,227 @@
-// #include <Arduino.h>
-// #include <WiFi.h>
-// #include <ESPAsyncWebServer.h>
-// #include <ArduinoJSon.h>
-// #include <Wire.h>
-// #include <LiquidCrystal_I2C.h>
-// #include <HX711.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJSon.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <HX711.h>
+#include <DFRobotDFPlayerMini.h>
+#include <EEPROM.h>
 
-// // WebSocket
-// const char *ssid = "BookTS";
-// const char *password = "12345678";
+#define LOADCELL_DOUT_PIN 14
+#define LOADCELL_SCK_PIN 12
+#define EEPROM_SIZE 16
 
-// // const char *ssid = "Yescoffee&tea2G";
-// // const char *password = "66668888";
+struct dataJSON
+{
+    int type;
+    int mangoCost;
+    int longanCost;
+    int durianCost;
+    float weight;
+};
 
-// AsyncWebServer server(80);
-// AsyncWebSocket ws("/ws");
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+dataJSON dataESP32;
+HX711 myLoadCell;
+DFRobotDFPlayerMini myDFPlayer;
+LiquidCrystal_I2C myLCD(0x27, 16, 2);
 
-// struct dataJSON
-// {
-//     int type;
-//     int mangoCost;
-//     int longanCost;
-//     int durianCost;
-//     float weight;
-// };
+const char *ssid = "BookTS";
+const char *password = "12345678";
+unsigned long timeOld = 0;
+unsigned long calibValue = 103918;
 
-// dataJSON dataESP32;
-// int status = 0;
+void Json_to_Value(String s);
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len);
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
+             AwsEventType type, void *arg, uint8_t *data, size_t len);
+void initWebSocket();
+void initLoadCell();
+void initMP3();
 
-// // LCD
-// LiquidCrystal_I2C myLCD(0x27, 16, 2);
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin();
+    myLCD.init();
+    myLCD.backlight();
+    myLCD.setCursor(0, 0);
+    myLCD.print("Cau hinh ...");
 
-// // Loadcell
-// #define LOADCELL_DOUT_PIN 16
-// #define LOADCELL_SCK_PIN 4
+    initLoadCell();
 
-// #define calibValue 103918
+    initMP3();
 
-// HX711 myLoadCell;
+    WiFi.begin(ssid, password);
 
-// // Time
-// unsigned long timeOld = 0;
+    for (size_t i = 0; i < 10; i++)
+    {
+        if (WiFi.status() != WL_CONNECTED)
+        {
+            myLCD.setCursor(9 + i % 3, 0);
+            myLCD.print(".  ");
+            if (i == 9)
+            {
+                myLCD.clear();
+                myLCD.setCursor(2, 0);
+                myLCD.print("That bai");
+            }
+            delay(1000);
+        }
+        else
+        {
+            Serial.println(WiFi.localIP());
+            myLCD.clear();
+            myLCD.setCursor(3, 0);
+            myLCD.print("Thanh cong");
+            myLCD.setCursor(1, 1);
+            myLCD.print(WiFi.localIP());
 
-// // function
-// void Json_to_Value(String s);
-// void Value_to_JSon();
-// void handleWebSocketMessage(void *arg, uint8_t *data, size_t len);
-// void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
-//              AwsEventType type, void *arg, uint8_t *data, size_t len);
-// void initWebSocket();
+            initWebSocket();
+            server.begin();
+            return;
+        }
+    }
+}
 
-// void initLoadCell();
+void loop()
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        delay(10);
+    }
+    char val[80];
+    dataESP32.weight = abs(myLoadCell.get_units(10));
+    sprintf(val, "%.2f", dataESP32.weight);
 
-// void setup()
-// {
-//     Serial.begin(115200);
-//     Wire.begin();
-//     myLCD.init();
-//     myLCD.backlight();
-//     myLCD.setCursor(0, 0);
-//     myLCD.print("Cau hinh ...");
+    myLoadCell.power_down();
 
-//     initLoadCell();
+    if (dataESP32.weight > 0.05)
+    {
+        Serial.println(val);
+        ws.cleanupClients();
+        ws.textAll(val);
 
-//     WiFi.begin(ssid, password);
+        myLCD.setCursor(0, 1);
+        myLCD.print("KL: " + String(val) + " kg");
+    }
+    else
+    {
+        myLCD.setCursor(0, 1);
+        myLCD.print("                ");
+    }
+    myLoadCell.power_up();
+}
 
-//     for (size_t i = 0; i < 10; i++)
-//     {
-//         if (WiFi.status() != WL_CONNECTED)
-//         {
-//             myLCD.setCursor(9 + i % 3, 0);
-//             myLCD.print(".  ");
-//             if (i == 9)
-//             {
-//                 myLCD.clear();
-//                 myLCD.setCursor(2, 0);
-//                 myLCD.print("That bai");
-//             }
-//             delay(1000);
-//         }
-//         else
-//         {
-//             Serial.println(WiFi.localIP());
-//             myLCD.clear();
-//             myLCD.setCursor(3, 0);
-//             myLCD.print("Thanh cong");
-//             myLCD.setCursor(1, 1);
-//             myLCD.print(WiFi.localIP());
+void initWebSocket()
+{
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
+}
 
-//             initWebSocket();
-//             server.begin();
-//             return;
-//         }
-//     }
-// }
+void Json_to_Value(String s)
+{
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, s);
 
-// void loop()
-// {
-//     if ((unsigned long)(millis() - timeOld) > 500)
-//     {
-//         char val[80];
-//         dataESP32.weight = abs(myLoadCell.get_units(10));
-//         sprintf(val, "%.2f", dataESP32.weight);
+    if (error)
+    {
+        Serial.println("Failed to parse JSON string.");
+    }
+    else
+    {
+        dataESP32.type = doc["type"];
+        dataESP32.mangoCost = doc["mangoCost"];
+        dataESP32.longanCost = doc["longanCost"];
+        dataESP32.durianCost = doc["durianCost"];
 
-//         myLoadCell.power_down();
-        
-//         if (dataESP32.weight > 0.05)
-//         {
-//             Serial.println(val);
-//             ws.cleanupClients();
-//             ws.textAll(val);
+        myDFPlayer.play(dataESP32.type);
+        delay(2500);
+        switch (dataESP32.type)
+        {
+        case 1:
+            myLCD.clear();
+            myLCD.setCursor(4, 0);
+            myLCD.print("NS: Xoai");
+            break;
+        case 2:
+            myLCD.clear();
+            myLCD.setCursor(4, 0);
+            myLCD.print("NS: Nhan");
+            break;
+        case 3:
+            myLCD.clear();
+            myLCD.setCursor(1, 0);
+            myLCD.print("NS: Sau Rieng");
+            break;
+        default:
+            break;
+        }
+        Serial.println(String(dataESP32.type) + " | " + String(dataESP32.mangoCost) + " | " + String(dataESP32.longanCost) + " | " + String(dataESP32.durianCost));
+    }
+}
 
-//             myLCD.setCursor(0, 1);
-//             myLCD.print(String(val) + " kg");
-//         }
-//         else {
-//             myLCD.setCursor(0, 1);
-//             myLCD.print("                ");
-//         }
-//         timeOld = millis();
-//         myLoadCell.power_up();
-//     }
-// }
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+{
+    AwsFrameInfo *info = (AwsFrameInfo *)arg;
+    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+    {
+        data[len] = 0;
+        Json_to_Value((char *)data);
+    }
+}
 
-// void initWebSocket()
-// {
-//     ws.onEvent(onEvent);
-//     server.addHandler(&ws);
-//     ;
-// }
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
+             AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+    switch (type)
+    {
+    case WS_EVT_CONNECT:
+        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+        myLCD.clear();
+        myLCD.setCursor(0, 0);
+        myLCD.print("Client Connected");
+        myDFPlayer.play(4);
+        delay(2500);
+        break;
+    case WS_EVT_DISCONNECT:
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+        myDFPlayer.play(4);
+        delay(2500);
+        break;
+    case WS_EVT_DATA:
+        handleWebSocketMessage(arg, data, len);
+        break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+        break;
+    }
+}
 
-// void Json_to_Value(String s)
-// {
-//     StaticJsonDocument<200> doc;
-//     DeserializationError error = deserializeJson(doc, s);
+void initLoadCell()
+{
+    myLoadCell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+    myLoadCell.set_scale(calibValue);
+    myLoadCell.tare();
+}
 
-//     if (error)
-//     {
-//         Serial.println("Failed to parse JSON string.");
-//     }
-//     else
-//     {
-//         dataESP32.type = doc["type"];
-//         dataESP32.mangoCost = doc["mangoCost"];
-//         dataESP32.longanCost = doc["longanCost"];
-//         dataESP32.durianCost = doc["durianCost"];
+void initMP3()
+{
+    Serial2.begin(9600);
 
-//         switch (dataESP32.type)
-//         {
-//         case 1:
-//             myLCD.clear();
-//             myLCD.setCursor(4, 0);
-//             myLCD.print("NS: Xoai");
-//             break;
-//         case 2:
-//             myLCD.clear();
-//             myLCD.setCursor(4, 0);
-//             myLCD.print("NS: Nhan");
-//             break;
-//         case 3:
-//             myLCD.clear();
-//             myLCD.setCursor(1, 0);
-//             myLCD.print("NS: Sau Rieng");
-//             break;
-//         default:
-//             break;
-//         }
+    if (!myDFPlayer.begin(Serial2))
+    {
+        Serial.println(F("Không thể khởi động:"));
+        Serial.println(F("1.Kiểm tra lại kết nối"));
+        Serial.println(F("2.Lắp lại thẻ nhớ"));
+        while (true)
+        {
+            delay(0);
+        }
+    }
+    Serial.println(F("DFPlayer Mini đang hoạt động"));
 
-//         Serial.println(String(dataESP32.type) + " | " + String(dataESP32.mangoCost) + " | " + String(dataESP32.longanCost) + " | " + String(dataESP32.durianCost));
-//     }
-// }
-
-// void Value_to_JSon()
-// {
-//     StaticJsonDocument<200> jsonVal;
-
-//     jsonVal["type"] = dataESP32.type;
-//     jsonVal["weight"] = dataESP32.weight;
-
-//     String jsonString;
-//     serializeJson(jsonVal, jsonString);
-
-//     ws.textAll(jsonString);
-// }
-
-// void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
-// {
-//     AwsFrameInfo *info = (AwsFrameInfo *)arg;
-//     if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
-//     {
-//         data[len] = 0;
-//         Json_to_Value((char *)data);
-//     }
-// }
-
-// void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
-//              AwsEventType type, void *arg, uint8_t *data, size_t len)
-// {
-//     switch (type)
-//     {
-//     case WS_EVT_CONNECT:
-//         Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-//         myLCD.clear();
-//         myLCD.setCursor(0, 0);
-//         myLCD.print("Client Connected");
-//         break;
-//     case WS_EVT_DISCONNECT:
-//         Serial.printf("WebSocket client #%u disconnected\n", client->id());
-//         break;
-//     case WS_EVT_DATA:
-//         handleWebSocketMessage(arg, data, len);
-//         break;
-//     case WS_EVT_PONG:
-//     case WS_EVT_ERROR:
-//         break;
-//     }
-// }
-
-// void initLoadCell()
-// {
-//     myLoadCell.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-
-//     myLoadCell.set_scale(calibValue);
-//     myLoadCell.tare();
-// }
+    myDFPlayer.volume(30);
+}
